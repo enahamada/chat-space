733e6322181fe136e93fa6873f9e931b46aec797
enahamada <chamaburiburi11.10@gmail.com>
2019-09-13 15:04:43 +0900

    reloadMessages

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,53 +1,29 @@
-$(function() {
+$(document).on('turbolinks:load', function(){
+
   function buildHTML(message){
-    var img ="";
-    if (message.image){
-    var img = `<img src = ${message.image} class: "lower-message_image">`
-    }
-    var html = `<div class="message">
-              <div class="upper-message">
-                <div class="upper-message__user-name">
-                  ${ message.user_name }
-                </div>
-                <div class="upper-message__date">
-                  ${ message.time }
-                </div>
-              </div>
-              <div class="lower-message">
-                <p class="lower-message__content">
-                  ${ message.content }
-                </p>
-              </div>
-            </div>`;
-  return html;
+    var img = message.image ? `<img class= "lower-message__image" src=${ message.image }>` : "";
+    var html = `<div class="message" data-message-id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__user">
+                      ${ message.user_name }
+                    </div>
+                    <div class="upper-message__date">
+                      ${ message.created_at }
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <div class="lower-message__content">
+                      ${ message.content }
+                    </div>
+                    ${ img }
+                  </div>
+                </div>`
+    return html;
   }
   function scroll() {
     $('.messages').animate({scrollTop: $('.message')[0].scrollHeight});
   }
   
-  var reloadMessages = function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-      var last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
-      // var group_id = $(".group").data("group-id");
-    $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      console.log('success');
-    })
-    .fail(function() {
-      console.log('error');
-    });
-   }
-  };
-
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -73,4 +49,33 @@ $(function() {
      $('.form__submit').prop('disabled', false);
    })
   })
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      var last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
+      // var group_id = $(".group").data("group-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      messages.forEach(function(message){
+        insertHTML = buildHTML(message);
+        $('#messages').append(insertHTML);
+      })
+      $('.main-hesader').animate({scrollTop: $('.main-header')[0].scrollHeight}, 'fast');
+    })
+    .fail(function() {
+      alert('error');
+    });
+   }
+  };
+  setInterval(reloadMessages, 5000);
 });
