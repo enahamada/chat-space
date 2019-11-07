768263629a0aebdf4e9eede7ee688bd2f633797f
enahamada <chamaburiburi11.10@gmail.com>
2019-11-07 14:10:39 +0900

    Update messages_controller_spec.rb

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -4,17 +4,46 @@ describe MessagesController do
   let(:group) { create(:group) }
   let(:user) { create(:user) }
 
+  describe '#index' do
+    context 'log in' do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'assigns @message' do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it 'assigns @group' do
+        expect(assigns(:group)).to eq group
+      end
+
+      it 'redners index' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'not log in' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
   describe '#create' do
     let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
 
     context 'log in' do
-      # この中にログインしている場合のテストを記述
       before do
         login user
       end
 
       context 'can save' do
-        # この中にメッセージの保存に成功した場合のテストを記述
         subject {
           post :create,
           params: params
@@ -31,7 +60,6 @@ describe MessagesController do
       end
 
       context 'can not save' do
-        # この中にメッセージの保存に失敗した場合のテストを記述
         let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
 
         subject {
@@ -51,7 +79,7 @@ describe MessagesController do
     end
 
     context 'not log in' do
-      # この中にログインしていない場合のテストを記述
+
       it 'redirects to new_user_session_path' do
         post :create, params: params
         expect(response).to redirect_to(new_user_session_path)
